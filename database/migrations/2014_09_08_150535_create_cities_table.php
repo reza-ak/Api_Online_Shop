958e34ed96c6e68d711e6e424c3328232b326a4e
reza-ak <reeza.dev@gmail.com>
2023-09-08 20:04:33 +0430

    Database creation (migration and model)

--- database/migrations/2014_09_08_150535_create_cities_table.php
+++ database/migrations/2014_09_08_150535_create_cities_table.php
@@ -11,20 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('address');
-            $table->string('cellphone');
-            $table->string('postal_code');
             $table->foreignId('province_id');
             $table->foreign('province_id')->references('id')->on('provinces')->onDelete('cascade');
-            $table->foreignId('city_id');
-            $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
-            $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();
         });
@@ -35,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cities');
     }
 };
