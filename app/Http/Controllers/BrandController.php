9626b7c02462f81d1a63190bc138a60ca13da4b5
reza-ak <reeza.dev@gmail.com>
2023-09-11 20:58:49 +0330

    list of brands and Show a brand (index, show)

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -10,68 +10,80 @@ use App\Http\Resources\BrandResource;
 use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Validator;
 
+use function PHPUnit\Framework\isNull;
+
 class BrandController extends ApiController
 {
-  /**
-   * Display a listing of the resource.
-   */
-  public function index()
-  {
-    //
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   */
-  public function store(Request $request)
-  {
-    $validator = Validator::make($request->all(), [
-      'name' => 'required',
-      'display_name' => 'required|unique:brands',
-    ]);
-    if ($validator->fails()) {
-      return $this->errorResponse($validator->messages(), 422);
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $brands = Brand::paginate(4);
+        return $this->successResponse([
+            'brands' => BrandResource::collection($brands),
+            'links' => BrandResource::collection($brands)->response()->getData()->links,
+            'meta' => BrandResource::collection($brands)->response()->getData()->meta,
+        ], 200);
     }
 
-    // زمانی که نیاز هست مثلا 2 یا بیشتر تغییرات در جدول داشته باشیم میتوانیم از transaction در mysql استفاده کنیم برای مثال مثلا اطلاعات در جدول order ذخیره شده و در جدول order_item هم ذخیره شده ولی وقتی اطلاعات میخواهد در جدول transaction ذخیره شود به مشکل میخورد و این اتفاق نمی افتد پس نباید دو جدول قبلی هم ذخیره سازی انجام میشد در این مواقع میتوان از transactoin در mysql استفاده کرد به صورت زیر
-    try {
-      DB::beginTransaction(); // شروع transaction
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'display_name' => 'required|unique:brands',
+        ]);
+        if ($validator->fails()) {
+            return $this->errorResponse($validator->messages(), 422);
+        }
 
-      $brand = Brand::create([
-        'name' => $request->name,
-        'display_name' => $request->display_name,
-      ]);
+        // زمانی که نیاز هست مثلا 2 یا بیشتر تغییرات در جدول داشته باشیم میتوانیم از transaction در mysql استفاده کنیم برای مثال مثلا اطلاعات در جدول order ذخیره شده و در جدول order_item هم ذخیره شده ولی وقتی اطلاعات میخواهد در جدول transaction ذخیره شود به مشکل میخورد و این اتفاق نمی افتد پس نباید دو جدول قبلی هم ذخیره سازی انجام میشد در این مواقع میتوان از transactoin در mysql استفاده کرد به صورت زیر
+        try {
+            DB::beginTransaction(); // شروع transaction
 
-      DB::commit(); // در صورت موفقیت تغییرات انجام میشود\
+            $brand = Brand::create([
+                'name' => $request->name,
+                'display_name' => $request->display_name,
+            ]);
 
-    } catch (Throwable $th) {
-      DB::rollBack();
-      return $this->errorResponse($th->getMessage(), 500);
+            DB::commit(); // در صورت موفقیت تغییرات انجام میشود\
+
+        } catch (Throwable $th) {
+            DB::rollBack();
+            return $this->errorResponse($th->getMessage(), 500);
+        }
+        return $this->successResponse(new BrandResource($brand), 201);
     }
-    return $this->successResponse(new BrandResource($brand), 201);
-  }
 
-  /**
-   * Display the specified resource.
-   */
-  public function show(string $id)
-  {
-    //
-  }
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $brand = Brand::find($id);
+        if(!is_null($brand)){
+            return $this->successResponse(new BrandResource($brand), 201);
+        }else{
+            return $this->errorResponse("Not Found", 404);
+        }
+    }
 
-  /**
-   * Update the specified resource in storage.
-   */
-  public function update(Request $request, string $id)
-  {
-    //
-  }
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
 
-  /**
-   * Remove the specified resource from storage.
-   */
-  public function destroy(string $id)
-  {
-    //
-  }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
--- app/Http/Resources/BrandResource.php
+++ app/Http/Resources/BrandResource.php
@@ -14,6 +14,10 @@ class BrandResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return[
+            'id' => $this->id,
+            'name' => $this->name,
+            'display_name' => $this->display_name,
+        ];
     }
 }
