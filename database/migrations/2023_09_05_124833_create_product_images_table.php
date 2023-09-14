54f6887cd50432a7f9e1f61f3bf9925604dd0341
reza-ak <reeza.dev@gmail.com>
2023-09-14 20:18:57 +0330

    Edit product image table (add softDelete)

--- database/migrations/2023_09_05_124833_create_product_images_table.php
+++ database/migrations/2023_09_05_124833_create_product_images_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('product_id');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->string('image');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
