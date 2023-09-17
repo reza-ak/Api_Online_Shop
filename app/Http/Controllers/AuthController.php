20d8f9bd4c53242e2ca8a0fedd6a963d8750dd56
reza-ak <reeza.dev@gmail.com>
2023-09-17 15:24:53 +0330

    Install sanctum Authentication (register, login, logout)

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends ApiController
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => "required|string",
+            'email' => "required|email|unique:users,email",
+            'password' => "required|string",
+            'c_password' => "required|same:password",
+            'address' => "required",
+            'cellphone' => "required",
+            'postal_code' => "required",
+            'province_id' => "required",
+            'city_id' => "required",
+        ]);
+
+        if ($validator->fails()) {
+            return $this->errorResponse($validator->messages(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'address' => $request->address,
+            'cellphone' => $request->cellphone,
+            'postal_code' => $request->postal_code,
+            'province_id' => $request->province_id,
+            'city_id' => $request->city_id,
+        ]);
+
+        $token = $user->createToken('myApp')->plainTextToken;
+        return $this->successResponse([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => "required|email",
+            'password' => "required|string",
+        ]);
+
+        if ($validator->fails()) {
+            return $this->errorResponse($validator->messages(), 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return $this->errorResponse("کاربری با این مشخصات پیدا نشد.", 401);
+        }
+
+        $token = $user->createToken('myApp')->plainTextToken;
+
+        return $this->successResponse([
+            'user' => $user,
+            'token' => $token
+        ], 200);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return $this->successResponse('logged out', 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\BrandController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,13 +19,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Authentication
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 // brands
 Route::apiResource('brands', BrandController::class);
-Route::get('/brands/{brands}/products', [BrandController::class, 'products']);
+Route::middleware('auth:sanctum')->get('/brands/{brands}/products', [BrandController::class, 'products']);
 
 // categories
 Route::apiResource('categories', CategoryController::class);
