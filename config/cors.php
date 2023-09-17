d6c2317a476a9675b75351973d4ab2dc65bc001a
reza-ak <reeza.dev@gmail.com>
2023-09-17 15:41:40 +0330

    CORS (educational)

--- config/cors.php
+++ config/cors.php
@@ -14,12 +14,17 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
+    // قانون same policy میگوید که فقط درخواست هایی که از یک منبع باشند اجرا میشود مثلا نباید پورت یا پروتکل یا زیر دامنه یا دامنه دیگری باشد
+    // برای حل این مشکل از CORS استفاده میشود که در request یک هدر با نام origin و مقدار سایتی که درخواست میدهد قرار میگیرد و در response یک هدر با نام Access-control-Allow-Origin و مقدار سایت هایی که اجازه دسترسی دارند قرار دارد که درصورت یکی بودن این دو هدر درخواست اجرا میشود و بلاک نمیشود
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    // در صورت نیاز میتوان از پکیج laravel cors هم استفاده کرد که امکانات بیشتری در اختیار ما قرار میدهد
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'], // در اینجا گفته شده که فقط برای مسیر هایی که با api شروع میشوند دستورات زیر را در نظر بگیرد
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*'], // در اینجا میتوان مشخص کرد که کدام سایت ها میتوانند دسترسی داشته باشند مثلا به صورت زیر
+    // 'allowed_origins' => ['https://webprog.io', 'https://www.test-cors.org'],
 
     'allowed_origins_patterns' => [],
 
